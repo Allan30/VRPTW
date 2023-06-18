0bff4dba285272e1334a1f987f0bb8ee889837e2
Thibault Thomas <thibaulthomas@gmail.com>
2023-06-18 15:32:32 +0200

    handle exceptions

--- VRPTW.UI/App.xaml.cs
+++ VRPTW.UI/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace VRPTW.UI;
 
@@ -7,4 +8,23 @@ namespace VRPTW.UI;
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        DispatcherUnhandledException += Application_DispatcherUnhandledException;
+    }
+
+    private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            MessageBox.Show(e.Exception.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            Shutdown(-1);
+        }
+        catch
+        {
+            Shutdown(-1);
+        }
+        
+    }
 }
