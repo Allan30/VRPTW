cb2220c092e0979904970b8a5fe076940d953c72
Thibault Thomas <thibaulthomas@gmail.com>
2023-06-18 16:40:30 +0200

    simple logging with serilog

--- VRPTW.UI/App.xaml.cs
+++ VRPTW.UI/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 using System.Windows.Threading;
+using Serilog;
+using VRPTW.UI.Views;
 
 namespace VRPTW.UI;
 
@@ -8,9 +12,30 @@ namespace VRPTW.UI;
 /// </summary>
 public partial class App : Application
 {
+    private readonly IHost _host;
+    
     public App()
     {
+        _host = Host
+            .CreateDefaultBuilder()
+            .UseSerilog((host, loggerConfig) =>
+                loggerConfig
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .MinimumLevel.Information())
+            .ConfigureServices(services => services.AddSingleton(s => new MainWindow()))
+            .Build();
+        
         DispatcherUnhandledException += Application_DispatcherUnhandledException;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+
+        base.OnStartup(e);
     }
 
     private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
@@ -18,7 +43,10 @@ public partial class App : Application
         try
         {
             e.Handled = true;
+            var logger = _host.Services.GetRequiredService<ILogger>();
+            logger.Fatal(e.Exception, "Unhandled exception");
             MessageBox.Show(e.Exception.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            logger.Fatal("Shutting down application.");
         }
         catch
         {
